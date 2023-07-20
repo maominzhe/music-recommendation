aef3a8f628e8d04e79aa2ddc621f6219bfd34d89
maominzhe <maominzhe93@gmail.com>
2023-07-20 17:18:53 +0200

    initial commit with data reader and recommender

---
+++ collaborative_filtering/data.py
@@ -0,0 +1,54 @@
+from pathlib import Path
+import scipy
+import pandas as pd
+
+def load_user_artists(user_artists_file: Path) -> scipy.sparse.csr_matrix:
+    """
+    load the user artists file and return a user-artists matirx in csr format
+
+    Args:
+        user_artists_file (Path): _description_
+
+    Returns:
+        scipy.sparse.csr_matrix: _description_
+    """
+    user_artists = pd.read_csv(user_artists_file, sep='\t')
+    user_artists.set_index(["userID", "artistID"], inplace=True)
+
+    coo = scipy.sparse.coo_matrix(
+        (user_artists.weight.astype(float),
+        (
+            user_artists.index.get_level_values(0),
+            user_artists.index.get_level_values(1),
+        ))
+    )
+
+    return coo.tocsr()
+
+class ArtistRetriever:
+    """This class gets the artist name from the artist ID."""
+
+    def __init__(self) -> None:
+        self._artists_df = None
+
+    def get_artist_name_from_id(self, artist_id: int) -> str:
+        """return the artist name from the artist ID."""
+        return self._artists_df.loc[artist_id, "name"]
+    
+    def load_artists(self, artists_file: Path) -> None:
+        """Load the artists file and stores it as a df in a private attribute"""
+        artists_df = pd.read_csv(artists_file, sep="\t")
+        artists_df = artists_df.set_index("id")
+        self._artists_df = artists_df
+
+
+if __name__ == "__main__":
+    user_artists_matrix = load_user_artists(
+        Path("../lastfmdata/user_artists.dat")
+    )
+    print(user_artists_matrix)
+
+    artist_retriever = ArtistRetriever()
+    artist_retriever.load_artists(Path("../lastfmdata/artists.dat"))
+    artist = artist_retriever.get_artist_name_from_id(1)
+    print(artist)
